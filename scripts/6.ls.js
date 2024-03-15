3795ea2126dae85bfa3d71605b98f898185bc7a0
Genner GR <lopezitzepcgenneralexander@gmail.com>
2024-03-15 20:50:56 +0000

    nothing

--- scripts/6.ls.js
+++ scripts/6.ls.js
@@ -2,15 +2,14 @@ const fs = require('node:fs')
 
 fs.readdir('.', (err, files) => {
     if (err) {
-        console.error('Error al leer el directorio: ', err)
-        return
+        console.error('Error al leer el directorio: ', err);
+        return;
     }
 
     files.forEach(file => {
-        console.log(file)
-    })
-})
-
+        console.log(file);
+    });
+});
 
 
 
